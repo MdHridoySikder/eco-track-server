3d1c2251a3727abde551816d4c053a284567bcfa
Md Hridoy Sikder <hridoy702345@gmail.com>
2026-01-19 07:37:04 +0600

    ADD GET API

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+// cwpP9DGjvSobfBno
+
+const uri =
+  "mongodb+srv://eco-track-db:cwpP9DGjvSobfBno@cluster0.su41mbr.mongodb.net/?appName=Cluster0";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const db = client.db("eco-track-db");
+    const modelCollection = db.collection("challenges");
+    // get method
+    // find
+    // find one
+    // Home page → only 4 challenges
+    app.get("/challenges/home", async (req, res) => {
+      const result = await modelCollection.find().limit(4).toArray();
+
+      res.send(result);
+    });
+
+    app.get("/challenges", async (req, res) => {
+      const result = await modelCollection.find().toArray();
+
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
